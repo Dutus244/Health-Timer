56f42bacdc2537cf98798c3fbdc16a62c59a8b9b
NhuY <81373090+congchuanhuy19@users.noreply.github.com>
2022-07-17 11:29:23 +0700

    doclist update

--- SourceCode/Server/WEB/doclist_script.js
+++ SourceCode/Server/WEB/doclist_script.js
@@ -23,3 +23,31 @@ function DocList(){
     };
     Http.send();
 }
+
+var addbutton = document.getElementById('addbutton');
+addbutton.addEventListener("click", addNewRow);
+var edit1row = true;
+
+function addNewRow(){
+    var table = document.getElementById("service");
+    var countrow = table.rows.length;
+    var id = "itcouldbesame" + countrow
+    var row = table.insertRow(countrow);
+    row.setAttribute("id", "row_"+id);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+
+    cell1.innerHTML =`<div class="iconsedit" style="background-color: white;">
+                            <i class="fas fa-pen" id='${id}' style="text-align: center"></i>
+                        </div>` ;
+    cell2.innerHTML = countrow;
+    cell3.innerHTML = "NULL";
+    cell4.innerHTML = "NULL";
+    cell5.innerHTML = "NULL";
+
+    var temp = document.getElementById(id);
+    temp.addEventListener('click',edit);
+}
