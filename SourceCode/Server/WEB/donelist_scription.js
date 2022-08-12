209e641b7bb49214ace44685bec7c99814a84b26
duchuynh1201 <102602729+duchuynh1201@users.noreply.github.com>
2022-08-13 01:46:02 +0700

    SearchBar (Waitlist + Donelist)

--- SourceCode/Server/WEB/donelist_scription.js
+++ SourceCode/Server/WEB/donelist_scription.js
@@ -1,5 +1,25 @@
 const url='http://localhost:803';
 
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("donelist");
+    tr = table.getElementsByTagName("tr");
+    
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+          if (td.innerText.toUpperCase().indexOf(filter) > -1)
+            tr[i].style.display = "";
+          else
+            tr[i].style.display = "none";
+        }
+    }
+}
+
 function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
